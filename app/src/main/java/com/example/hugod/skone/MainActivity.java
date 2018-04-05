db01830cde5b4161a69a64a18acc72d5aa2d7731
CrispyFruits <hugodahl99@hotmail.com>
2018-04-05 20:49:04 +0200

    Added Placeholders and Buttons
    Added Placeholders, x Button and check Button. No real functionality added.

--- app/src/main/java/com/example/hugod/skone/MainActivity.java
+++ app/src/main/java/com/example/hugod/skone/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.hugod.skone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageButton xBtn = findViewById(R.id.xButton);
+        xBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                //X Button functionality
+                return false;
+            }
+        });
+
+        ImageButton checkBtn = findViewById(R.id.checkButton);
+        checkBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                //Check Button functionality
+                return false;
+            }
+        });
+
     }
+
+
 }
