f6ad86efbf8a9afa50986ab1584b0939b509272c
CrispyFruits <hugodahl99@hotmail.com>
2018-04-10 08:38:47 +0200

    Updates

--- app/src/main/java/com/example/hugod/skone/MainActivity.java
+++ app/src/main/java/com/example/hugod/skone/MainActivity.java
@@ -1,38 +1,75 @@
 package com.example.hugod.skone;
 
+import android.annotation.SuppressLint;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String youScore;
+    private String phoneScore;
+    private final String TAG = "Activity";
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Resources res = getResources();
 
+        final TextView trick = (TextView) findViewById(R.id.trick);
+        //Sets scores to ""
+        final TextView youLetters = (TextView) findViewById(R.id.youSkate);
+        youLetters.setText(youScore);
+
+        final TextView phoneLetters = (TextView) findViewById(R.id.phoneSkate);
+        phoneLetters.setText(phoneScore);
+
+        if(getString(R.string.trick).equals("Trick")){
+            trick.setText(getRandomTrick());
+        }
 
         ImageButton xBtn = findViewById(R.id.xButton);
-        xBtn.setOnTouchListener(new View.OnTouchListener() {
+        xBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
+            public void onClick(View view) {
                 //X Button functionality
-                return false;
+                youScore = addLetter(youScore);
+                youLetters.setText(youScore);
+                trick.setText(getRandomTrick());
+
+
             }
         });
 
         ImageButton checkBtn = findViewById(R.id.checkButton);
-        checkBtn.setOnTouchListener(new View.OnTouchListener() {
+        checkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                //Check Button functionality
-                return false;
+            public void onClick(View view) {
+                //Check Button functionality TODO: Fix score system SKATE, Win and lose screen
+                phoneScore = addLetter(phoneScore);
+                phoneLetters.setText(phoneScore);
+                trick.setText(getRandomTrick());
+
+
             }
         });
 
+    }
+
+    public String getRandomTrick(){
+        String currentTrick;
+        String[] tricks = getResources().getStringArray(R.array.basic_tricks);
+
+        currentTrick = tricks[(int)(Math.random()*tricks.length)];
+
+        return currentTrick;
     }
 
     public String addLetter(String currentScore){
