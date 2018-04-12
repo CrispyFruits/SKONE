a5d11f15c810f1ae9918c767b9d5ff620fc0c382
CrispyFruits <hugodahl99@hotmail.com>
2018-04-12 10:57:34 +0200

    Updates

--- app/src/main/java/com/example/hugod/skone/MainActivity.java
+++ app/src/main/java/com/example/hugod/skone/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.hugod.skone;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private String youScore = "";
     private String phoneScore = "";
     private final String TAG = "Activity";
+
+
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -31,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
         final TextView phoneLetters = (TextView) findViewById(R.id.phoneSkate);
         phoneLetters.setText(phoneScore);
 
-        if(getString(R.string.trick).equals("Trick")){
+        final AlertDialog winDialog = createWinDialog();
+        final AlertDialog loseDialog = createLoseDialog();
+
+        if (getString(R.string.trick).equals("Trick")) {
             trick.setText(getRandomTrick());
         }
 
@@ -43,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 youScore = addLetter(youScore);
                 youLetters.setText(youScore);
                 trick.setText(getRandomTrick());
-
+                checkWinCondition(winDialog, youScore);
 
             }
         });
@@ -52,31 +60,38 @@ public class MainActivity extends AppCompatActivity {
         checkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Check Button functionality TODO: Fix score system SKATE, Win and lose screen
+                //Check Button functionality
                 phoneScore = addLetter(phoneScore);
                 phoneLetters.setText(phoneScore);
                 trick.setText(getRandomTrick());
-
+                checkWinCondition(loseDialog, phoneScore);
 
             }
         });
 
     }
 
-    public String getRandomTrick(){
+    private void checkWinCondition(AlertDialog dialog, String score) {
+        if (score.equals("S.K.A.T.E")) {
+            dialog.show();
+        }
+
+    }
+
+    public String getRandomTrick() {
         String currentTrick;
         String[] tricks = getResources().getStringArray(R.array.basic_tricks);
 
-        currentTrick = tricks[(int)(Math.random()*tricks.length)];
+        currentTrick = tricks[(int) (Math.random() * tricks.length)];
 
         return currentTrick;
     }
 
-    public String addLetter(String currentScore){
+    public String addLetter(String currentScore) {
         String theScore = "";
 
 
-        switch (currentScore){
+        switch (currentScore) {
             case "":
                 theScore = "S";
                 break;
@@ -95,10 +110,64 @@ public class MainActivity extends AppCompatActivity {
 
             case "S.K.A.T":
                 theScore = "S.K.A.T.E";
+                //TODO: output who wins and loses!
                 break;
         }
 
         return theScore;
     }
+
+    public AlertDialog createLoseDialog(){
+        AlertDialog dialog;
+
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, 2);
+
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.dialog_message)
+                .setTitle(R.string.dialog_title_win);
+        builder.setCancelable(false);
+
+
+        builder.setPositiveButton(R.string.dialog_play_again_btn, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                recreate();
+            }
+        });
+        builder.setNegativeButton(R.string.quit_btn, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+
+        return  dialog = builder.create();
+    }
+
+    public AlertDialog createWinDialog(){
+        AlertDialog dialog;
+
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, 2);
+
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.dialog_message)
+                .setTitle(R.string.dialog_title_lose);
+        builder.setCancelable(false);
+
+
+        builder.setPositiveButton(R.string.dialog_play_again_btn, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                recreate();
+            }
+        });
+        builder.setNegativeButton(R.string.quit_btn, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+
+        return  dialog = builder.create();
+    }
+
 
 }
